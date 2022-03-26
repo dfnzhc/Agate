8b3e1f2167f9b2702507f67f9da6281fe29b1a5a
yutou <z69500jqk@qq.com>
2022-03-26 17:50:59 +0800

    framework setting

---
+++ include/CGT/CGT.h
@@ -0,0 +1,11 @@
+﻿//
+// Created by 秋鱼头 on 2022/3/26.
+//
+
+#pragma once
+
+#ifdef ENABLE_CUDA
+
+#else
+
+#endif
---
+++ include/pch.h
@@ -0,0 +1,37 @@
+﻿//
+// Created by 秋鱼头 on 2022/3/26.
+//
+
+#pragma once
+#if defined(_MSC_VER)
+/* Disable some warnings on MSVC++ */
+#pragma warning(disable : 4127 4702 4100 4515 4800 4146 4512)
+#define WIN32_LEAN_AND_MEAN     /* Don't ever include MFC on Windows */
+#define NOMINMAX                /* Don't override min/max */
+#endif
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <fstream>
+#include <execution>
+#include <filesystem>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <stdexcept>
+#include <queue>
+#include <stack>
+#include <array>
+#include <numeric>
+#include <map>
+#include <Windows.h>
+#include <winnt.h>
+#include <windowsx.h>
+#include <wrl.h>
+#include <limits>
+#include <set>
+#include <ranges>
