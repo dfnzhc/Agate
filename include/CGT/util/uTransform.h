a1569c19e66a0dcacb1bbd916c8d15ac72cd58d4
yutou <z69500jqk@qq.com>
2022-03-28 17:48:29 +0800

    更新变换方法

--- include/CGT/util/uTransform.h
+++ include/CGT/util/uTransform.h
@@ -5,10 +5,39 @@
 #pragma once
 
 #include <CGT/common.h>
+#include "uMath.h"
 namespace CGT {
 
-/// build another base vectors from a
-void CoordinateSystem(const Vector3f &a, Vector3f &b, Vector3f &c);
+inline double cos_theta(const Vector3d w) {
+    return w.z;
+}
+
+inline double abs_cos_theta(const Vector3d w) {
+    return fabs(w.z);
+}
+
+inline double sin_theta2(const Vector3d w) {
+    return fmax(0.0, 1.0 - cos_theta(w) * cos_theta(w));
+}
+
+inline double sin_theta(const Vector3d w) {
+    return sqrt(sin_theta2(w));
+}
+
+inline double cos_phi(const Vector3d w) {
+    double sinTheta = sin_theta(w);
+    if (sinTheta == 0.0) return 1.0;
+    return Clamp(w.x / sinTheta, -1.0, 1.0);
+}
+
+inline double sin_phi(const Vector3d w) {
+    double sinTheta = sin_theta(w);
+    if (sinTheta) return 0.0;
+    return Clamp(w.y / sinTheta, -1.0, 1.0);
+}
+
+/// create object space from the normal vector
+Matrix3x3d CoordinateSystem(const Vector3d& n);
 
 } // namespace CGT
 
--- src/util/uTransform.cpp
+++ src/util/uTransform.cpp
@@ -6,14 +6,29 @@
 
 namespace CGT {
 
-void CoordinateSystem(const Vector3f &a, Vector3f &b, Vector3f &c) {
-    if (std::abs(a.x) > std::abs(a.y)) {
-        float invLen = 1.0f / std::sqrt(a.x * a.x + a.z * a.z);
-        c = Vector3f{ a.z * invLen, 0.0f, -a.x * invLen };
-    } else {
-        float invLen = 1.0f / std::sqrt(a.y * a.y + a.z * a.z);
-        c = Vector3f{ 0.0f, a.z * invLen, -a.y * invLen };
-    }
-    b = glm::cross(c, a);
+Matrix3x3d CoordinateSystem(const Vector3d& n)
+{
+    Vector3d z = Vector3d(n.x, n.y, n.z);
+    Vector3d h = z;
+    if (fabs(h.x) <= fabs(h.y) && fabs(h.x) <= fabs(h.z))
+        h.x = 1.0;
+    else if (fabs(h.y) <= fabs(h.x) && fabs(h.y) <= fabs(h.z))
+        h.y = 1.0;
+    else
+        h.z = 1.0;
+
+    glm::normalize(z);
+    Vector3d y = cross(h, z);
+    glm::normalize(y);
+    Vector3d x = cross(z, y);
+    glm::normalize(x);
+
+    Matrix3x3d o2w;
+    o2w[0] = x;
+    o2w[1] = y;
+    o2w[2] = z;
+    
+    return o2w;
 }
+
 } // namespace CGT
